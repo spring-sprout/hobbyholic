315458936c10e5165819d33006fabdf043a59efc
arawn <arawn.kr@gmail.com>
2013-01-31 14:40:30 +0900

    JavaConfig에서 properties 값을 처리하는 방법 변경

--- src/main/java/org/springsprout/hobbyholic/config/AppConfig.java
+++ src/main/java/org/springsprout/hobbyholic/config/AppConfig.java
@@ -1,20 +1,15 @@
 package org.springsprout.hobbyholic.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
@@ -22,70 +17,63 @@ import java.util.Properties;
 
 /**
  * @author Keesun Baik
+ * @author Yongkwon Park
  */
 @Configuration
-@ComponentScan(basePackages = "org.springsprout.hobbyholic",
-        excludeFilters = {@ComponentScan.Filter(Controller.class), @ComponentScan.Filter(Configuration.class)})
-@PropertySource(value = "/hobbyholic.properties")
+@ComponentScan(basePackages="org.springsprout.hobbyholic"
+             , excludeFilters={@ComponentScan.Filter(Controller.class), @ComponentScan.Filter(Configuration.class)})
+@PropertySource(name="hobbyholicProperties", value="/hobbyholic.properties")
 public class AppConfig {
-
-    @Autowired org.springframework.core.env.Environment environment;
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
-        PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
-        Resource[] resourceLocations = new Resource[] {
-            new ClassPathResource("/hobbyholic.properties"),
-        };
-        pspc.setLocations(resourceLocations);
-        return pspc;
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
     @Bean
-    public DataSource dataSource() {
-        ComboPooledDataSource dataSource = null;
-        try {
-            dataSource = new ComboPooledDataSource();
-            dataSource.setUser(environment.getProperty("db.user"));
-            dataSource.setPassword(environment.getProperty("db.password"));
-            dataSource.setDriverClass(environment.getProperty("db.driver"));
-            dataSource.setJdbcUrl(environment.getProperty("db.url"));
-            dataSource.setAcquireIncrement(Integer.parseInt(environment.getProperty("db.pool.acquireIncrement")));
-            dataSource.setMinPoolSize(Integer.parseInt(environment.getProperty("db.pool.minPoolSize")));
-            dataSource.setMaxPoolSize(Integer.parseInt(environment.getProperty("db.pool.maxPoolSize")));
-            dataSource.setMaxIdleTime(Integer.parseInt(environment.getProperty("db.pool.maxIdleTime")));
-            dataSource.setInitialPoolSize(Integer.parseInt(environment.getProperty("db.pool.initialPoolSize")));
-        } catch (PropertyVetoException e) {
-            throw new IllegalArgumentException(e);
-        }
+    public DataSource dataSource(@Value("${db.driver}") String driverClass
+                               , @Value("${db.url}") String jdbcUrl
+                               , @Value("${db.user}") String user
+                               , @Value("${db.password}") String password
+                               , @Value("${db.pool.acquireIncrement}") Integer acquireIncrement
+                               , @Value("${db.pool.minPoolSize}") Integer minPoolSize
+                               , @Value("${db.pool.maxPoolSize}") Integer maxPoolSize
+                               , @Value("${db.pool.maxIdleTime}") Integer maxIdleTime
+                               , @Value("${db.pool.initialPoolSize}") Integer initialPoolSize) throws PropertyVetoException {
+
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass(driverClass);
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+        dataSource.setAcquireIncrement(acquireIncrement);
+        dataSource.setMinPoolSize(minPoolSize);
+        dataSource.setMaxPoolSize(maxPoolSize);
+        dataSource.setMaxIdleTime(maxIdleTime);
+        dataSource.setInitialPoolSize(initialPoolSize);
+
         return dataSource;
     }
 
     @Bean
-    public SessionFactory sessionFactory(){
-//        org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
-//        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
-//
-//        return new LocalSessionFactoryBuilder(dataSource())
-//                .scanPackages("org.springsprout.hobbyholic.domain")
-//                .addProperties(hibernateProperties())
-//                .buildSessionFactory(serviceRegistry);
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource
+                                                , @Value("${hibernate.dialect}") String dialect
+                                                , @Value("${hibernate.hbm2ddl.auto}") String hbm2ddlAuto
+                                                , @Value("${hibernate.show_sql}") String showSql
+                                                , @Value("${hibernate.format_sql}") String formatSql){
+
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put(Environment.DIALECT, dialect);
+        hibernateProperties.put(Environment.SHOW_SQL, hbm2ddlAuto);
+        hibernateProperties.put(Environment.HBM2DDL_AUTO, showSql);
+        hibernateProperties.put(Environment.FORMAT_SQL, formatSql);
 
         LocalSessionFactoryBean lsfb = new LocalSessionFactoryBean();
-        lsfb.setDataSource(dataSource());
-        lsfb.setHibernateProperties(hibernateProperties());
+        lsfb.setDataSource(dataSource);
+        lsfb.setHibernateProperties(hibernateProperties);
         lsfb.setPackagesToScan("org.springsprout.hobbyholic.domain");
-        return lsfb.getObject();
-    }
 
-    @Bean
-    public Properties hibernateProperties() {
-        Properties properties = new Properties();
-        properties.put(Environment.DIALECT, environment.getProperty(Environment.DIALECT));
-        properties.put(Environment.SHOW_SQL, environment.getProperty(Environment.SHOW_SQL));
-        properties.put(Environment.HBM2DDL_AUTO, environment.getProperty(Environment.HBM2DDL_AUTO));
-        properties.put(Environment.FORMAT_SQL, environment.getProperty(Environment.FORMAT_SQL));
-        return properties;
+        return lsfb;
     }
 
 }
